6942bad06a85dbc13b17d4141099944f715798dc
thcnnn <epoyanarmen@gmail.com>
2024-04-03 16:50:27 +0300

    task3

--- server.py
+++ server.py
@@ -1,27 +1,34 @@
 import socket
-
 port=9090
 sock = socket.socket()
 sock.bind(('', port))
 print("Server is starting")
-sock.listen(0)
+sock.listen(1)
 print("Port",port,"is listing")
 #conn, addr = sock.accept()
 print("Client is accepted")
 # print("Client adress:",addr[0])
 # print("Client port:",addr[1])
 
-msg = ''
-
 while True:
-	conn, addr = sock.accept()
-	print("Client adress:", addr[0])
-	print("Client port:", addr[1])
-	data = conn.recv(1024)
-	if data.decode() == 'exit':
-		break
-	msg = data.decode()
-	conn.send(msg.upper().encode())
-	print(msg)
-	conn.close()
+    conn, addr = sock.accept()
+    print("Client address:", addr[0])
+    print("Client port:", addr[1])
+
+    msg = ''
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+
+        msg = data.decode()
+        if msg == 'exit':
+            break
+
+        conn.send(msg.upper().encode())
+        print(msg)
+
+    print("Client disconnected")
+    conn.close()
+
 
