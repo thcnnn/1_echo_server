c2aea652286cfa3f60484f8845cc0033e87afb0d
thcnnn <epoyanarmen@gmail.com>
2024-04-03 16:56:33 +0300

    task45

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 import socket
 from time import sleep
-
+port = input('port no: ')
+try:
+    port = int(port)
+except ValueError:
+    print("Invalid port number. Using default port 9090.")
+    port = 9090
+host_name = input('host_name: ') or 'vboxuser'
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('localhost', 9090))
+sock.connect(('', 9090))
 
-msg = input("Your string:")
+msg = input("Your string: ")
 #msg = "Hi!"
 sock.send(msg.encode())
 
--- server.py
+++ server.py
@@ -1,34 +1,39 @@
 import socket
-port=9090
+from datetime import datetime
+port=input('port no: ')
+try:
+    port = int(port)
+except ValueError:
+    print("Invalid port number. Using default port 9090.")
+    port = 9090
 sock = socket.socket()
 sock.bind(('', port))
-print("Server is starting")
+with open('logs', 'a') as file:
+	file.write(("Server is starting | " + str(datetime.now())))
+	file.write('\n')
 sock.listen(1)
-print("Port",port,"is listing")
-#conn, addr = sock.accept()
-print("Client is accepted")
-# print("Client adress:",addr[0])
-# print("Client port:",addr[1])
+with open('logs', 'a') as file:
+	file.write(("Port" + str(port) + "is listing | " + str(datetime.now())))
+	file.write('\n')
+	file.write(("Client is accepted | "+ str(datetime.now())))
+	file.write('\n')
+#print(socket.gethostname())
+msg = ''
 
 while True:
-    conn, addr = sock.accept()
-    print("Client address:", addr[0])
-    print("Client port:", addr[1])
+	conn, addr = sock.accept()
+	with open('logs', 'a') as file:
+		file.write(("Client adress:" + (addr[0]) + ' | ' + str(datetime.now())))
+		file.write('\n')
+		file.write(("Client port:" + str(addr[1]) + '| ' + str(datetime.now())))
+		file.write('\n')
+	data = conn.recv(1024)
+	if data.decode() == 'exit':
+		break
+	msg = data.decode()
+	conn.send(msg.upper().encode())
+	print(msg)
+	conn.close()
 
-    msg = ''
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-
-        msg = data.decode()
-        if msg == 'exit':
-            break
-
-        conn.send(msg.upper().encode())
-        print(msg)
-
-    print("Client disconnected")
-    conn.close()
 
 
