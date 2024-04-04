497fe81e007c48cf204cda4a1a4bb997519782e5
thcnnn <epoyanarmen@gmail.com>
2024-04-04 13:38:24 +0300

    task11

--- client.py
+++ client.py
@@ -1,27 +1,29 @@
 import socket
-from time import sleep
-port = 9090
-sock = socket.socket()
-#sock.setblocking(0)
-sock.connect(('127.0.0.1', 9090))
+import threading
+import os
 
-response = sock.recv(1024).decode()
-print(response)
 
-while True:
-    data = input()
-    sock.send(data.encode())
-    response = sock.recv(1024).decode()
-    print(response)
+def listen(s: socket.socket):
+    while True:
+        msg = s.recv(2048)
+        print('\r\r' + msg.decode('ascii') + '\n' + f'you: ', end='')
 
-    if response == "Представьтесь.":
-        name = input("Введите ваше имя: ")
-        sock.send(name.encode())
-    elif response == "Укажите пароль.":
-        print(response == "Укажите пароль.")
-        password = input("Введите пароль: ")
-        sock.send(password.encode())
-    elif response == "Аутентификация успешна." or response == "Регистрация успешна." or "Ошибка аутентификации. Неверный пароль.":
-        break
 
-sock.close()
+def connect(host='127.0.0.1', port=9090):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    s.connect((host, port))
+
+    threading.Thread(target=listen, args=(s,), daemon=True).start()
+
+    s.send('__join'.encode('ascii'))
+
+    while True:
+        msg = input(f'you: ')
+        s.send(msg.encode('ascii'))
+
+
+if __name__ == '__main__':
+    os.system('clear')
+    print('Welcome to chat!')
+    connect()
--- server.py
+++ server.py
@@ -1,60 +1,38 @@
 import socket
-import json
-import hashlib
-port=9090
-sock = socket.socket()
-sock.bind(('', port))
-print("Server is starting")
-sock.listen(1)
-print("Port",port,"is listing")
-def load_users():
-	try:
-		with open("users.json", "r") as file:
-			return json.load(file)
-	except FileNotFoundError:
-		return {}
-def save_users(user_ip, user_name, user_pass):
-	with open("users.json", "r") as file:
-		data = json.load(file)
-		data[user_ip] = {"name": user_name, "password": user_pass}
-		with open("users.json", "w") as outfile:
-			json.dump(data, outfile, indent=2)
 
-msg = ''
-def handle_client(conn, addr):
-	users = load_users()
-	client_ip = addr[0]
 
-	if client_ip in users:
-		conn.send(f"Добрый день, {users[client_ip]['name']}!\n".encode())
-		conn.send("Введите пароль: ".encode())
-		password = conn.recv(1024).decode().strip()
 
-		hashed_password = hashlib.sha256(password.encode()).hexdigest()
+def listen(host='127.0.0.1', port=9090):
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-		if hashed_password == users[client_ip].get("password"):
-			conn.send("Аутентификация успешна.".encode())
-		else:
-			conn.send("Ошибка аутентификации. Неверный пароль.".encode())
+	s.bind((host, port))
+	print(f'Listening at {host}:{port}')
 
-	else:
-		conn.send("Представьтесь: ".encode())
-		name = conn.recv(1024).decode().strip()
-		conn.send("Укажите пароль: ".encode())
-		password = conn.recv(1024).decode().strip()
-		conn.send("Регистрация успешна.".encode())
-		hashed_password = hashlib.sha256(password.encode()).hexdigest()
+	members = []
+	while True:
+		msg, addr = s.recvfrom(2048)
 
-		users[client_ip] = {"name": name, "password": hashed_password}
-		save_users(str(client_ip), name, hashed_password)
+		if addr not in members:
+			members.append(addr)
 
-	conn.close()
-while True:
-	conn, addr = sock.accept()
-	print("Client is accepted")
-	print("Client adress:", addr[0])
-	print("Client port:", addr[1])
-	handle_client(conn, addr)
+		if not msg:
+			continue
+
+		client_id = addr[1]
+		if msg.decode('ascii') == '__join':
+			print(f'Client {client_id} joined chat')
+			continue
+
+		msg = f'client{client_id}: {msg.decode("ascii")}'
+		for member in members:
+			if member == addr:
+				continue
+
+			s.sendto(msg.encode('ascii'), member)
+
+
+if __name__ == '__main__':
+	listen()
 
 
 
