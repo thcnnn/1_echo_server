dbf81d7b806d488501102eea841d24385a919439
thcnnn <epoyanarmen@gmail.com>
2024-04-04 12:30:02 +0300

    task 78

--- client.py
+++ client.py
@@ -1,20 +1,27 @@
 import socket
 from time import sleep
-port = input('port no: ')
-try:
-    port = int(port)
-except ValueError:
-    print("Invalid port number. Using default port 9090.")
-    port = 9090
-host_name = input('host_name: ') or 'vboxuser'
+port = 9090
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('', 9090))
+#sock.setblocking(0)
+sock.connect(('127.0.0.1', 9090))
 
-msg = input("Your string: ")
-#msg = "Hi!"
-sock.send(msg.encode())
+response = sock.recv(1024).decode()
+print(response)
 
-data = sock.recv(1024)
+while True:
+    data = input()
+    sock.send(data.encode())
+    response = sock.recv(1024).decode()
+    print(response)
+
+    if response == "Представьтесь.":
+        name = input("Введите ваше имя: ")
+        sock.send(name.encode())
+    elif response == "Укажите пароль.":
+        print(response == "Укажите пароль.")
+        password = input("Введите пароль: ")
+        sock.send(password.encode())
+    elif response == "Аутентификация успешна." or response == "Регистрация успешна." or "Ошибка аутентификации. Неверный пароль.":
+        break
+
 sock.close()
-print(data.decode())
--- server.py
+++ server.py
@@ -1,39 +1,60 @@
 import socket
-from datetime import datetime
-port=input('port no: ')
-try:
-    port = int(port)
-except ValueError:
-    print("Invalid port number. Using default port 9090.")
-    port = 9090
+import json
+import hashlib
+port=9090
 sock = socket.socket()
 sock.bind(('', port))
-with open('logs', 'a') as file:
-	file.write(("Server is starting | " + str(datetime.now())))
-	file.write('\n')
+print("Server is starting")
 sock.listen(1)
-with open('logs', 'a') as file:
-	file.write(("Port" + str(port) + "is listing | " + str(datetime.now())))
-	file.write('\n')
-	file.write(("Client is accepted | "+ str(datetime.now())))
-	file.write('\n')
-#print(socket.gethostname())
-msg = ''
+print("Port",port,"is listing")
+def load_users():
+	try:
+		with open("users.json", "r") as file:
+			return json.load(file)
+	except FileNotFoundError:
+		return {}
+def save_users(user_ip, user_name, user_pass):
+	with open("users.json", "r") as file:
+		data = json.load(file)
+		data[user_ip] = {"name": user_name, "password": user_pass}
+		with open("users.json", "w") as outfile:
+			json.dump(data, outfile, indent=2)
 
+msg = ''
+def handle_client(conn, addr):
+	users = load_users()
+	client_ip = addr[0]
+
+	if client_ip in users:
+		conn.send(f"Добрый день, {users[client_ip]['name']}!\n".encode())
+		conn.send("Введите пароль: ".encode())
+		password = conn.recv(1024).decode().strip()
+
+		hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+		if hashed_password == users[client_ip].get("password"):
+			conn.send("Аутентификация успешна.".encode())
+		else:
+			conn.send("Ошибка аутентификации. Неверный пароль.".encode())
+
+	else:
+		conn.send("Представьтесь: ".encode())
+		name = conn.recv(1024).decode().strip()
+		conn.send("Укажите пароль: ".encode())
+		password = conn.recv(1024).decode().strip()
+		conn.send("Регистрация успешна.".encode())
+		hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+		users[client_ip] = {"name": name, "password": hashed_password}
+		save_users(str(client_ip), name, hashed_password)
+
+	conn.close()
 while True:
 	conn, addr = sock.accept()
-	with open('logs', 'a') as file:
-		file.write(("Client adress:" + (addr[0]) + ' | ' + str(datetime.now())))
-		file.write('\n')
-		file.write(("Client port:" + str(addr[1]) + '| ' + str(datetime.now())))
-		file.write('\n')
-	data = conn.recv(1024)
-	if data.decode() == 'exit':
-		break
-	msg = data.decode()
-	conn.send(msg.upper().encode())
-	print(msg)
-	conn.close()
+	print("Client is accepted")
+	print("Client adress:", addr[0])
+	print("Client port:", addr[1])
+	handle_client(conn, addr)
 
 
 
